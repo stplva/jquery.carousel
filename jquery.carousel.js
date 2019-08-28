87cef8c4f09c22e7778b7699354348818874d188
Sofiia Teplova <sofiia.teplova@gmail.com>
2019-08-29 00:14:35 +0300

    Added css and js files

---
+++ jquery.carousel.js
@@ -0,0 +1,69 @@
+// This is a simple jQuery carousel plugin
+// It was made by Sofiia Teplova.
+
+(function($) {
+
+  $.fn.carousel = function(options) {
+
+    //SETTINGS
+    var defaults = {
+      speed: 700,
+      background: 'white',
+      pictureWidth: 1000,
+      pictureMargin: 100,
+    };
+
+    var settings = $.extend(defaults, options);
+
+
+    //VARS
+    var $arrowPrev = $('.carousel-arrow-prev'),
+      $arrowNext = $('.carousel-arrow-next'),
+      $carouselUl = $('.carousel-ul'),
+
+      $picturesNumber = $carouselUl.find('.carousel-li').length,
+
+      $pictureWidth = $('.carousel-li img').css('width'),
+      $pictureMargin = $('.carousel-li').css('marginRight'),
+
+      $pixelOffset = settings.pictureWidth + settings.pictureMargin,
+      $maxOffset = 0,
+      $minOffset = -($picturesNumber - 1) * $pixelOffset,
+      $currentLeftValue = 0;
+
+
+    //STYLES
+    $('.carousel-hider').css('width', settings.pictureWidth);
+    $carouselUl.find('.carousel-li').css({
+      'background': settings.background,
+      'marginRight': settings.pictureMargin
+    });
+    $carouselUl.find('.carousel-li img').css('width', settings.pictureWidth);
+
+
+    // ACTUALLY CAROUSEL
+    $arrowNext.on('click', function(i) {
+      if ($currentLeftValue > $minOffset) {
+
+        $currentLeftValue -= $pixelOffset;
+        $carouselUl.animate({
+          left: $currentLeftValue + 'px'
+        }, settings.speed)
+
+      }
+    });
+
+    $arrowPrev.on('click', function(i) {
+      if ($currentLeftValue < $maxOffset) {
+
+        $currentLeftValue += $pixelOffset;
+        $carouselUl.animate({
+          left: $currentLeftValue + 'px'
+        }, settings.speed)
+
+      }
+    });
+
+  }
+
+})(jQuery);
